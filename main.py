3a4c7c86c62c690a9b72966a9d65fce226b87cc8
seoin0110 <seoinseoinseoin@naver.com>
2023-07-11 07:37:15 +0000

    feat: add translator before text encoding #3

--- main.py
+++ main.py
@@ -10,25 +10,49 @@ import torch
 from clip.text_encoder import TextEncoder
 from torchvision.transforms import CenterCrop, Compose, Normalize, Resize, ToTensor
 
+from dotenv import load_dotenv
 
-
+import urllib.request
+import json
 
 app = FastAPI()
 
 PTL_DIR = './models/'
+
+load_dotenv()
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get('/favicon.ico')
-async def favicon():
+def favicon():
     file_path = os.path.join(app.root_path, "static/favicon.ico")
     return FileResponse(file_path)
 
 @app.get("/tokenize/{text}")
 def text_encoder(text : str):
-    _text_encoder = TextEncoder(text)
+    # text 번역
+    encText = urllib.parse.quote(text)
+    data = "source=ko&target=en&text=" + encText
+    url = "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation"
+    request = urllib.request.Request(url)
+    request.add_header("X-NCP-APIGW-API-KEY-ID",client_id)
+    request.add_header("X-NCP-APIGW-API-KEY",client_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        translated_text = json.loads(response_body.decode('utf-8'))["message"]["result"]["translatedText"]
+    else:
+        print("Error Code:" + rescode)
+    if translated_text:
+        _text_encoder = TextEncoder(translated_text)
+    else:
+        _text_encoder = TextEncoder(text)
     return _text_encoder
 
 @app.get("/{filename}")
